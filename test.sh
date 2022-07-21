af5828dde59f4d1e43dba4db61bc0f03545cd039
南山竹 <junjiemars@gmail.com>
2022-07-21 16:58:43 +0800

    unused

--- test.sh
+++ test.sh
@@ -123,6 +123,7 @@ test_c_program () {
 #include <stdio.h>
 
 int main(void) {
+  __pragma(warning(suppress:4100 4101 4189)) int x = 0;
   printf("sizeof(fpos_t) = %zu\n", sizeof(fpos_t));
   return 0;
 }
