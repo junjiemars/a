d929363b7551d75e8342ed37cdecf157c0579058
南山竹 <junjie.mars@icloud.com>
2022-07-23 20:51:32 +0800

    posix: msvc C2208

--- test.sh
+++ test.sh
@@ -149,6 +149,23 @@ test_c_program () {
 #  endif
 #endif
 
+#include <stdint.h>
+
+
+#if (MSVC)
+typedef __int8            int8_t;
+typedef unsigned __int8   uint8_t;
+typedef __int16           int16_t;
+typedef unsigned __int16  uint16_t;
+typedef __int32           int32_t;
+typedef unsigned __int32  uint32_t;
+typedef __int64           int64_t;
+typedef unsigned __int64  uint64_t;
+#endif
+
+#include <BaseTsd.h>
+
+
 static void fn(void);
 
 int main(void) {
